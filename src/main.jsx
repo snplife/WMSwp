04f01532dc3b1d31fb988a0da875d57d1a8a79b2
snplife <brunovskya@gmail.com>
2026-03-05 14:13:21 +0100

    Force clear service workers and Cache Storage on app startup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+if (typeof window !== "undefined") {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((registration) => {
+        registration.unregister();
+      });
+    });
+  }
+
+  if ("caches" in window) {
+    caches.keys().then((keys) => {
+      keys.forEach((key) => {
+        caches.delete(key);
+      });
+    });
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
